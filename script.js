2cca1f2e84e0924af33065a57f43f69bbdceaf56
FraRodrigues <fraamrodrigues@gmail.com>
2022-08-19 19:10:41 +0100

    Added function that changes result on scoreboard and function that limits game to 5 rounds

--- script.js
+++ script.js
@@ -1,84 +1,9 @@
 
-/*console.log("Game started: Round 1");
-
-console.log("Rock, Paper or Scissor?");
-
-function computerPlay() {
-    let computerSelection = Math.floor(Math.random()*3)+1
-
-    return computerSelection;
-}
-
-function playerPlay() {
-    let playerSelection = prompt("Choose Rock, Paper or Scissor")
-    playerSelection = playerSelection.toLowerCase()
-    //console.log(playerSelection)
-    if (playerSelection === "rock") {
-        playerSelection = 1
-    } else if (playerSelection === "paper") {
-        playerSelection = 2
-    } else if (playerSelection === "scissor") {
-        playerSelection = 3
-    } else {
-        prompt("Error!!! Choose Rock, Paper or Scissor")
-    }
-
-    //console.log(playerSelection)
-
-    return playerSelection;
-}
-
-function battle(playerSelection, computerSelection){
-    if (computerSelection === playerSelection) {
-        console.log("Draw")
-        playerWin = 0
-    } else if (computerSelection === 1 && playerSelection === 2) {
-        roundMessage("You win! Paper beats rock")
-        playerWin = 1
-    } else if (computerSelection === 2 && playerSelection === 1) {
-        roundMessage("You lose! Paper beats rock")
-        playerWin = -1 
-    } else if (computerSelection === 1 && playerSelection === 3) {
-        roundMessage("You lose! Rock beats Scissors")
-        playerWin = -1 
-    } else if (computerSelection === 3 && playerSelection === 1) {
-        roundMessage("You win! Rock beats Scissors") 
-        playerWin = 1
-    } else if (computerSelection === 2 && playerSelection === 3) {
-        roundMessage("You win! Scissors beats paper") 
-        playerWin = 1
-    } else if (computerSelection === 3 && playerSelection === 2) {
-        roundMessage("You lose! Scissors beats paper") 
-        playerWin = -1
-    }
-
-    return playerWin;
-}
-
-function game() {
-    wins = 0
-    for (let i = 0; i < 5; i++) {
-    let playerSelection = playerPlay()
-    let computerSelection = computerPlay()
-    let playerWin = battle(playerSelection,computerSelection)
-    wins = wins + playerWin
-    }
-
-    if (wins < 0) {
-        console.log("You lost the game after the five rounds")
-    } else if (wins = 0) {
-        console.log("The game ended in a draw! Try again!")
-    } else if (wins > 0) {
-        console.log("You won the game after the five rounds")
-    }
-} */
-
-
 const rock = 1;
 const paper = 2;
 const scissor = 3;
 
-function computerPlay() {
+function computerPlay() {   //random number 1-3 assigned to computer
     let computerSelection = Math.floor(Math.random()*3)+1
 
     return computerSelection;
@@ -93,7 +18,7 @@ function roundMessage(string){
     message.style.margin = "3vh 0 3vh 0";
 }
 
-function changeImage(playerSelection, computerSelection){
+function changeImage(playerSelection, computerSelection){ //shows image of what player and COM played
 
     const player = document.getElementById("player");
     const computer = document.getElementById("COM");
@@ -114,42 +39,75 @@ function changeImage(playerSelection, computerSelection){
     }
 }
 
-function battle(playerSelection, computerSelection){
+function battle(playerSelection, computerSelection){   
     if (computerSelection === playerSelection) {
         roundMessage("Draw");
-        playerWin = 0
+        battleResult = 0
     } else if (computerSelection === 1 && playerSelection === 2) {
         roundMessage("You win! Paper beats rock")
-        playerWin = 1
+        battleResult = 1
     } else if (computerSelection === 2 && playerSelection === 1) {
         roundMessage("You lose! Paper beats rock")
-        playerWin = -1 
+        battleResult = -1 
     } else if (computerSelection === 1 && playerSelection === 3) {
         roundMessage("You lose! Rock beats Scissors")
-        playerWin = -1 
+        battleResult = -1 
     } else if (computerSelection === 3 && playerSelection === 1) {
         roundMessage("You win! Rock beats Scissors") 
-        playerWin = 1
+        battleResult = 1
     } else if (computerSelection === 2 && playerSelection === 3) {
         roundMessage("You win! Scissors beats paper") 
-        playerWin = 1
+        battleResult = 1
     } else if (computerSelection === 3 && playerSelection === 2) {
         roundMessage("You lose! Scissors beats paper") 
-        playerWin = -1
+        battleResult = -1
     }
 
     changeImage(playerSelection, computerSelection);
     
-    return playerWin;
+    return battleResult;
 }
 
+let playerWins = 0;
+let comWins = 0;
+
+function changeResult(winner){  //changes Result on webpage
+    let playerResult = document.querySelector(".playerResult");
+    let comResult = document.querySelector(".comResult");
+
+    if (winner == 1){
+        playerWins = playerWins + 1;
+        playerResult.innerHTML = playerWins;
+    } else if (winner == -1){
+        comWins = comWins + 1;
+        comResult.innerHTML = comWins;
+    }
+}
 
 document.getElementById("paper").addEventListener("click", (e) => {game(paper)});
 document.getElementById("rock").addEventListener("click", (e) => {game(rock)});
 document.getElementById("scissor").addEventListener("click", (e) => {game(scissor)});
 
-function game(playerSelection) {
-    let computerSelection = computerPlay()
-    let playerWin = battle(playerSelection,computerSelection)
+let round = 0;
 
+function game(playerSelection) {
+    round++;
+    if (round < 5){
+    let computerSelection = computerPlay();
+    let battleResult = battle(playerSelection,computerSelection);
+    changeResult(battleResult);
+    } else if (round == 5){
+        let computerSelection = computerPlay();
+        let battleResult = battle(playerSelection,computerSelection);
+        changeResult(battleResult);
+        if(comWins == playerWins){
+            alert("Game ended on a Draw! Try again?");
+        } else if (comWins < playerWins) {
+            alert("You won the Game! Play again?");
+        } else {
+            alert("You lost the Game! Try again?");
+        }
     }
+    
+    console.log(round);
+}
